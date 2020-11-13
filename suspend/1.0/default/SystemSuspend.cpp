811566daf08587f97e26ee458ab3ab0a63947f1d
Benjamin Schwartz <bsschwar@google.com>
2020-11-13 11:18:47 -0800

    suspend: Implement wakeup stats collection
    Bug: 170588437
    Test: adb shell dumpsys suspend_control_internal --wakeups
    Test: atest SystemSuspendV1_0UnitTest
    
    Change-Id: If790627e8ed3c00ceb3903fbac1a43e2ff70f80e

--- suspend/1.0/default/SystemSuspend.cpp
+++ suspend/1.0/default/SystemSuspend.cpp
@@ -147,10 +147,12 @@ SystemSuspend::SystemSuspend(unique_fd wakeupCountFd, unique_fd stateFd, unique_
       mControlService(controlService),
       mControlServiceInternal(controlServiceInternal),
       mStatsList(maxNativeStatsEntries, std::move(kernelWakelockStatsFd)),
+      mWakeupList(1000),
       mUseSuspendCounter(useSuspendCounter),
       mWakeLockFd(-1),
       mWakeUnlockFd(-1),
-      mWakeupReasonsFd(std::move(wakeupReasonsFd)) {
+      mWakeupReasonsFd(std::move(wakeupReasonsFd)),
+      mAutosuspendEnabled(false) {
     mControlServiceInternal->setSuspendService(this);
 
     if (!mUseSuspendCounter) {
@@ -166,14 +168,13 @@ SystemSuspend::SystemSuspend(unique_fd wakeupCountFd, unique_fd stateFd, unique_
 }
 
 bool SystemSuspend::enableAutosuspend() {
-    static bool initialized = false;
-    if (initialized) {
+    if (mAutosuspendEnabled) {
         LOG(ERROR) << "Autosuspend already started.";
         return false;
     }
 
     initAutosuspend();
-    initialized = true;
+    mAutosuspendEnabled = true;
     return true;
 }
 
@@ -260,6 +261,7 @@ void SystemSuspend::initAutosuspend() {
             std::vector<std::string> wakeupReasons = readWakeupReasons(mWakeupReasonsFd);
             mControlService->notifyWakeup(success, wakeupReasons);
 
+            mWakeupList.update(wakeupReasons);
             updateSleepTime(success, suspendTime.suspendTime);
         }
     });
@@ -317,6 +319,10 @@ const WakeLockEntryList& SystemSuspend::getStatsList() const {
 
 void SystemSuspend::updateStatsNow() {
     mStatsList.updateNow();
+}
+
+const WakeupList& SystemSuspend::getWakeupList() const {
+    return mWakeupList;
 }
 
 /**
--- suspend/1.0/default/SystemSuspend.h
+++ suspend/1.0/default/SystemSuspend.h
@@ -28,6 +28,7 @@
 
 #include "SuspendControlService.h"
 #include "WakeLockEntryList.h"
+#include "WakeupList.h"
 
 namespace android {
 namespace system {
@@ -103,6 +104,7 @@ class SystemSuspend : public ISystemSuspend {
     bool enableAutosuspend();
     bool forceSuspend();
 
+    const WakeupList& getWakeupList() const;
     const WakeLockEntryList& getStatsList() const;
     void updateWakeLockStatOnRelease(const std::string& name, int pid, TimestampType timeNow);
     void updateStatsNow();
@@ -134,6 +136,7 @@ class SystemSuspend : public ISystemSuspend {
     sp<SuspendControlServiceInternal> mControlServiceInternal;
 
     WakeLockEntryList mStatsList;
+    WakeupList mWakeupList;
 
     // If true, use mSuspendCounter to keep track of native wake locks. Otherwise, rely on
     // /sys/power/wake_lock interface to block suspend.
@@ -142,6 +145,8 @@ class SystemSuspend : public ISystemSuspend {
     unique_fd mWakeLockFd;
     unique_fd mWakeUnlockFd;
     unique_fd mWakeupReasonsFd;
+
+    bool mAutosuspendEnabled;
 };
 
 }  // namespace V1_0
