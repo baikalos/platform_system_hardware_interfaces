0d387d60efb8f0fe0bedbf5336bbdbaa1531c850
Benjamin Schwartz <bsschwar@google.com>
2020-11-13 11:18:47 -0800

    suspend: Implement wakeup stats collection
    Bug: 170588437
    Test: adb shell dumpsys suspend_control_internal --wakeups
    Change-Id: If790627e8ed3c00ceb3903fbac1a43e2ff70f80e

---
+++ suspend/1.0/default/WakeupList.cpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "WakeupList.h"
+
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
+namespace android {
+namespace system {
+namespace suspend {
+namespace V1_0 {
+
+WakeupList::WakeupList(size_t capacity) : mCapacity(capacity) {}
+
+void WakeupList::getWakeupStats(std::vector<WakeupInfo>* wakeups) const {
+    std::scoped_lock lock(mLock);
+
+    for (const WakeupInfo& w : mWakeups) {
+        wakeups->push_back(w);
+    }
+}
+
+void WakeupList::update(const std::vector<std::string>& wakeupReasons) {
+    if (wakeupReasons.empty()) {
+        LOG(ERROR) << "WakeupList: empty wakeup reasons";
+        return;
+    }
+    std::string key = ::android::base::Join(wakeupReasons, ";");
+
+    std::scoped_lock lock(mLock);
+
+    auto it = mLookupTable.find(key);
+    if (it == mLookupTable.end()) {
+        // Create a new entry
+        WakeupInfo newEntry;
+        newEntry.name = key;
+        newEntry.count = 1;
+        insertEntry(newEntry);
+    } else {
+        // Entry found. Increment the count
+        auto staleEntry = it->second;
+        WakeupInfo updatedEntry = *staleEntry;
+        updatedEntry.count++;
+
+        deleteEntry(staleEntry);
+        insertEntry(std::move(updatedEntry));
+    }
+}
+
+void WakeupList::evictIfFull() {
+    if (mWakeups.size() == mCapacity) {
+        deleteEntry(std::prev(mWakeups.end()));
+        LOG(ERROR) << "WakeupList: Capacity met, consider adjusting capacity to "
+                      "avoid stats eviction.";
+    }
+}
+
+void WakeupList::insertEntry(WakeupInfo entry) {
+    evictIfFull();
+    mWakeups.emplace_front(std::move(entry));
+    mLookupTable[entry.name] = mWakeups.begin();
+}
+
+void WakeupList::deleteEntry(std::list<WakeupInfo>::iterator entry) {
+    mLookupTable.erase(entry->name);
+    mWakeups.erase(entry);
+}
+
+}  // namespace V1_0
+}  // namespace suspend
+}  // namespace system
+}  // namespace android
